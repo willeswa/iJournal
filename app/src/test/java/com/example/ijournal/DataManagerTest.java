67a715ed0047efcfa8ac647fdb84a5ff3044b527
wanjala <gwiliez@gmail.com>
2020-06-02 17:17:38 +0300

    Intro to Testing - Unit testing - Automated UI testing

---
+++ app/src/test/java/com/example/ijournal/DataManagerTest.java
@@ -0,0 +1,40 @@
+package com.example.ijournal;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataManagerTest {
+    static DataManager sDataManager;
+
+    @BeforeClass
+    public static void classSetup(){
+        sDataManager = DataManager.getInstance();
+    }
+
+    @Before
+    public void setUp(){
+        sDataManager.getNotes().clear();
+        sDataManager.initializeExampleNotes();
+    }
+
+    @Test
+    public void createNewNote() {
+        final CourseInfo course = sDataManager.getCourse("android_async");
+        final String noteTitle = "Test Note Title";
+        final  String noteText = "Test note text description";
+
+        int noteIndex = sDataManager.createNewNote();
+        NoteInfo newNote = sDataManager.getNotes().get(noteIndex);
+        newNote.setCourse(course);
+        newNote.setTitle(noteTitle);
+        newNote.setText(noteText);
+
+        NoteInfo compareNote = sDataManager.getNotes().get(noteIndex);
+        assertEquals(compareNote.getText(), noteText);
+        assertEquals(compareNote.getTitle(), noteTitle);
+        assertEquals(compareNote.getCourse(), course);
+    }
+}
